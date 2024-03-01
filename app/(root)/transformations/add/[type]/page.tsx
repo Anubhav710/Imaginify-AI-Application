319acb67fe17f0cc98d634408c45e1f6b9f88c19
Anubhav <anubhavagrawal710@gmail.com>
2024-03-01 17:12:11 +0530

    First Commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,34 @@
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.action";
+
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!userId) alert("User Not Found");
+
+  const user = userId;
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          userId={user!}
+          action="Add"
+          type={transformation.type as TransformationTypeKey}
+        />
+      </section>
+    </>
+  );
+};
+
+export default AddTransformationTypePage;
